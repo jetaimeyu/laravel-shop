0cf16cd4dabb4803bee437f33c2f24a44411e3ed
yuguangfu <2923409299@qq.com>
2020-04-15 17:45:10 +0800

    32'

--- app/Models/UserAddress.php
+++ app/Models/UserAddress.php
@@ -17,4 +17,19 @@ class UserAddress extends Model
         'contact_phone',
         'last_used_at',
     ];
+    protected $dates=[
+        'last_used_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+
+    }
+
+    public function getFullAddressAttribute()
+    {
+        return "{$this->province}{$this->city}{$this->district}{$this->address}";
+
+    }
 }
